b0fb3eda648c2a66314cc157250179065dddaa1c
neiaaa <rizky.nurahman48@gmail.com>
2021-12-22 15:38:42 -0800

    pkl

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,31 @@ class UserSeeder extends Seeder
         $user = new User();
         $user->name = 'Rizky Nurahman';
         $user->email = 'rizky@gmail.com';
-        $user->password = Hash::make('Rahasia');
+        $user->password = Hash::make('1');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Rico Achmad';
+        $user->email = 'rico@gmail.com';
+        $user->password = Hash::make('2');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Zulfan M';
+        $user->email = 'zulfan@gmail.com';
+        $user->password = Hash::make('3');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Syukur S';
+        $user->email = 'syukur@gmail.com';
+        $user->password = Hash::make('4');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Aziz TB';
+        $user->email = 'aziz@gmail.com';
+        $user->password = Hash::make('5');
         $user->save();
     }
 }
