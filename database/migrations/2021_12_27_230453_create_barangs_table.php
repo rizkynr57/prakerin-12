1c51955f89ce0630bbec5c6b129022862afa319e
neya18 <rizky.nurahman48@gmail.com>
2022-01-03 13:38:12 +0700

    Update 2021_12_27_230453_create_barangs_table.php

--- database/migrations/2021_12_27_230453_create_barangs_table.php
+++ database/migrations/2021_12_27_230453_create_barangs_table.php
@@ -15,8 +15,8 @@ class CreateBarangsTable extends Migration
     {
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('id_barang_masuk')->unsigned();
-            $table->foreign('id_barang_masuk')->references('id')->on('barang_masuks');
+           
+           
             $table->string('nama_barang');
             $table->string('jenis_barang');
             $table->integer('jumlah_barang');
