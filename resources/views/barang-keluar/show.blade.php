ebf664e0317455f6921ee5c2253bee6b1dd55b36
neya18 <rizky.nurahman48@gmail.com>
2022-01-04 13:52:44 +0700

    Update show.blade.php

--- resources/views/barang-keluar/show.blade.php
+++ resources/views/barang-keluar/show.blade.php
@@ -17,24 +17,24 @@
                 <div class="card-header">Show</div>
                 <div class="card-body">
                     <div class="form-group">
-                        <label for="">Nama Supplier</label>
-                        <input type="text" name="nama" class="form-control" value="{{ $supplier->nama_supplier }}" readonly>
+                        <label for="">ID Barang</label>
+                        <input type="text" name="alamat" class="form-control" value="{{ $barangMasuk->id_barang }}" readonly>
                     </div>
                     <div class="form-group">
-                        <label for="">Alamat</label>
-                        <input type="text" name="alamat" class="form-control" value="{{ $supplier->alamat }}" readonly>
+                        <label for="">Jumlah Pengiriman</label>
+                        <input type="text" name="jumlah" class="form-control" value="{{ $barangMasuk->jumlah_pengiriman }}">
                     </div>
                     <div class="form-group">
-                        <label for="">Nomor Telepon</label>
-                        <input type="text" name="no_telp" class="form-control" value="{{ $supplier->no_telp }}" readonly>
+                        <label for="">Tujuan</label>
+                        <input type="text" name="tujuan" class="form-control" value="{{ $barangMasuk->tujuan }}">
                     </div>
                     <div class="form-group">
-                        <label for="">Nama Perusahaan</label>
-                        <input type="text" name="perusahaan" class="form-control" value="{{ $supplier->nama_perusahaan }}" readonly>
+                        <label for="">Tanggal Pengiriman Barang</label>
+                        <input type="text" name="tgl_pengiriman" class="form-control" value="{{ $barangMasuk->tgl_pengiriman }}">
                     </div>
                     <div class="form-group">
                         <br>
-                        <a href="{{ route('supplier.index') }}" class="btn btn-outline-primary">Kembali</a>
+                        <a href="{{ route('barang-masuk.index') }}" class="btn btn-outline-primary">Kembali</a>
                     </div>
                 </div>
             </div>
