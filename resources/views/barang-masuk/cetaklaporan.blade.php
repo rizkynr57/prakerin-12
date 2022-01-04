c79a49ce990324c9843c436e5ac36973e52ded1f
neya18 <rizky.nurahman48@gmail.com>
2022-01-04 09:00:26 +0700

    Update cetaklaporan.blade.php

--- resources/views/barang-masuk/cetaklaporan.blade.php
+++ resources/views/barang-masuk/cetaklaporan.blade.php
@@ -1,1 +1,34 @@
-
+<head>
+	<title>Cetak Laporan</title>
+	<style>
+		table.static {
+			position: relative;
+			border: 1px solid $543535
+		}
+</head>
+<body>
+	<div class="form-group">
+		<p align="center"><b>Laporan Data Pemasukan</b></p>
+		<table class="static" align="center" rules="all" border="1px" width="95%">
+			<tr>
+				<th>No</th>
+				<th>Nama Supplier</th>
+				<th>Nama Barang</th>
+				<th>Jenis Barang</th>
+				<th>Jumlah Barang</th>
+				<th>Satuan</th>
+				
+				@foreach($barangMasuk as $data)
+				       <tr>
+					          <td>{{ $no++ }}</td>
+					          <td>{{ $data->supplier->nama_supplier }}</td>
+					          <td>{{ $data->barang->nama_barang }}</td>
+					          <td>{{ $data->jenis_barang }} </td>
+					          <td>{{ $data->jumlah_barang }}</td>
+					          <td>{{ $data->satuan }}</td>
+					</tr>
+				@endforeach
+				</tr>
+		    </table>
+		</div>
+</body>
