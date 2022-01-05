2464b90be9a4c3c4a491bf5595fe60eb320edb40
reign90 <rizky.nurahman48@gmail.com>
2022-01-05 23:46:49 +0700

    Update SupplierController.php

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -10,6 +10,11 @@ use Illuminate\Http\Request;
 class SupplierController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('role:admin');
+    }
+
     public function index()
     {
         $supplier = Supplier::all();
