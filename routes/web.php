acbac10473aa029e1fd16fd21a78b0f5db8abf97
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 00:44:32 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,13 +31,13 @@ Route::group(['middleware' => ['auth']], function(){
 
     Route::resource('barang-keluar', BarangKeluarController::class);
     Route::get('/laporan-barangkeluar-all', BarangKeluarController@laporanBarangKeluarAll)->name('laporanBarangKeluarAll');
-    Route::get('/laporan-barangkeluar/{id}, BarangKeluarController@laporanBarangKeluar)->name('laporanBarangKeluar');
+    Route::get('/laporan-barangkeluar/{id}', BarangKeluarController@laporanBarangKeluar)->name('laporanBarangKeluar');
     Route::get('/cetak-pdf-all', BarangKeluarController@cetakPDF_all)->name('exportPDF.barangKeluarAll');
     Route::get('/cetak-pdf/{id}', BarangKeluarController@cetakPDF)->name('exportPDF.barangKeluar');
 
     Route::resource('barang-masuk', BarangMasukController::class);
     Route::get('/laporan-barangmasuk-all', BarangMasukController@laporanBarangMasukAll)->name('laporanBarangMasukAll');
-    Route::get('/lapiran-barangmasuk/{id}, BarangMasukController@laporanBarangMasuk)->name('laporanBarangMasuk');
+    Route::get('/lapiran-barangmasuk/{id}', BarangMasukController@laporanBarangMasuk)->name('laporanBarangMasuk');
     Route::get('/cetak-pdf-all', BarangMasukController@cetakPDF_all)->name('exportPDF.barangMasukAll');
     Route::get('/cetak-pdf/{id}', BarangMasukController@cetakPDF)->name('exportPDF.barangMasuk');
 });
