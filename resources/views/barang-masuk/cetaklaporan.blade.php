2cbe70705a0ca349bfa4ebe52b38b6fc2479dd2d
neya18 <rizky.nurahman48@gmail.com>
2022-01-05 11:04:45 +0700

    Update cetaklaporan.blade.php

--- resources/views/barang-masuk/cetaklaporan.blade.php
+++ resources/views/barang-masuk/cetaklaporan.blade.php
@@ -1,35 +1,46 @@
-<head>
-	<title>Cetak Laporan</title>
-	<style>
-		table.static {
-			position: relative;
-			border: 1px solid #543535
-		}
-         </style>
-</head>
-<body>
-	<div class="form-group">
-		<p align="center"><b>Laporan Data Pemasukan</b></p>
-                <a type="button" href="{{ route('barang-masuk.cetakPDF') }}" class="btn btn-success">Export on PDF</a>
-		<table class="static" align="center" rules="all" border="1px" width="95%">
-			<tr>
-				<th>No</th>
-				<th>Nama Supplier</th>
-				<th>Nama Barang</th>
-				<th>Jenis Barang</th>
-				<th>Jumlah Barang</th>
-				<th>Satuan</th>
-		          </tr>
-				@foreach($barangMasuk as $data)
-				       <tr>
-					          <td>{{ $no++ }}</td>
-					          <td>{{ $data->supplier->nama_supplier }}</td>
-					          <td>{{ $data->barang->nama_barang }}</td>
-					          <td>{{ $data->jenis_barang }} </td>
-					          <td>{{ $data->jumlah_barang }}</td>
-					          <td>{{ $data->satuan }}</td>
-					</tr>
-				@endforeach
-		    </table>
-		</div>
-</body>
+<style>
+    #product-keluar {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+    #product-keluar td, #product-keluar th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+    #product-keluar tr:nth-child(even){background-color: #f2f2f2;}
+    #product-keluar tr:hover {background-color: #ddd;}
+    #product-keluar th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
+</style>
+
+<table id="product-keluar" width="100%">
+    <thead>
+    <tr>
+        <td>No</td>
+        <td>Nama Supplier</td>
+        <td>Nama Barang</td>
+        <td>Jenis Barang</td>
+        <td>Jumlah Pemasukan</td>
+        <td>Tanggal Pemasukan Barang</td>
+    </tr>
+    </thead>
+    @foreach(barangMasuk as $data)
+        <tbody>
+        <tr>
+            <td>{{ $no++ }}</td>
+            <td>{{ $data->supplier->nama_supplier }}</td>
+            <td>{{ $data->barang->nama_barang}}</td>
+            <td>{{ $data->jenis_pengiriman}}</td>
+            <td>{{ $data->jumlah_barang }}</td>
+            <td>{{ $data->tgl_masuk }}</td>
+        </tr>
+        </tbody>
+    @endforeach
+
+</table>
