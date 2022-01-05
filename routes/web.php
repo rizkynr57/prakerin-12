e4a167df2b69365172722ffbf464fa974a865488
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 00:25:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\BarangKeluarController;
 use App\Http\Controllers\BarangMasukController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('auth.login');
@@ -26,9 +18,9 @@ Auth::routes(
     ]
 );
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', HomeController@index])->name('home');
 
-
+// Hak akses admin dan petugas dibatasi melalui controller
 
 Route::group(['middleware' => ['auth']], function(){
    
