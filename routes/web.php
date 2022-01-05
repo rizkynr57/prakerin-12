c9c285d4ea6f245e9059759977de5a3e95455fbd
reign90 <rizky.nurahman48@gmail.com>
2022-01-05 23:29:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 
-Route::group(['prefix', 'middleware' => ['auth']], function(){
+Route::group(['middleware' => ['auth']], function(){
    
     Route::resource('supplier', SupplierController::class);
     Route::post('/cetak-supplier', SupplierController@cetakSupplierPDF);
