4d924fc436202492d833164a04a96b2493220ee9
reign90 <rizky.nurahman48@gmail.com>
2022-01-05 12:21:15 +0700

    Update SupplierController.php

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -9,11 +9,7 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $supplier = Supplier::all();
