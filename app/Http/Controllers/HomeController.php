1e99d9fbf3e8f70fb37ee0c31b2f97112085c6b3
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 21:33:27 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $totalMasuk = DB::table('barang_masuk')->get()->sum('jumlah_penerimaan');
+        $totalMasuk = DB::table('barang_masuk')->get()->sum('jumlah_pemasukan');
         $totalKeluar = DB::table('barang_keluar')->get()->sum('jumlah_pengiriman');
         return view('home')->with('jumlah_penerimaan', 'jumlah_pengiriman', $totalMasuk, $totalKeluar);
     }
