2cbc41fe7bd3e1384b57bbccde637ea9cb668397
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 18:53:47 +0700

    Update laporanBarangMasuk.blade.php

--- resources/views/barang-masuk/laporanBarangMasuk.blade.php
+++ resources/views/barang-masuk/laporanBarangMasuk.blade.php
@@ -20,7 +20,9 @@
 </style>
 
 <table id="barang-masuk" width="100%">
-   <a type="button" href="{{ route('exportPDF.barangMasuk') }} class="btn btn-success">Export Ke PDF</a>
+   <a type="button" href="{{ route('exportPDF.barangMasuk') }} class="btn btn-success">
+       <i class="fas fa-file-export">Export PDF</i>
+   </a>
     <thead>
     <tr>
         <td>No</td>
