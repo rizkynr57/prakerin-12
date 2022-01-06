ea65afaf494038d64bca8aa9ce7314c5acba2ca7
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 19:01:04 +0700

    Update index.blade.php

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -21,7 +21,7 @@
                 <div class="card-body">
                     <div class="table-responsive">
                         <table class="table" id="supplier">
-                           <a type="button" href="{{ route('exportPDF.SuppliersAll') }} class="btn btn-success">
+                           <a type="button" href="{{ route('exportPDF.SuppliersAll') }}" class="btn btn-success">
                                <i class="fas fa-file-export">Cetak data dan Export PDF</i>
                            </a>
                             <thead>
