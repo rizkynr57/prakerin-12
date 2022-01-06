46b7396b1a62cb6ab1301be7f0c3410d04c5196c
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 21:19:13 +0700

    Update Home.php

--- app/Models/Home.php
+++ app/Models/Home.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Session;
+
+class Home extends Model
+{
+    use HasFactory;
+
+    public function barangMasuk()
+    {
+        return $this->hasMany('App\Models\Barang_masuk');
+    }
+    public function BarangKeluar()
+    {
+        return $this->hasMany('app\Models\Barang_keluar');
+    }
+}
