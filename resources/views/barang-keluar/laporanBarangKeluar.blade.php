cce8bce4c85894c08b828c276122bdb4d8047007
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 08:33:49 +0700

    Update laporanBarangKeluar.blade.php

--- resources/views/barang-keluar/laporanBarangKeluar.blade.php
+++ resources/views/barang-keluar/laporanBarangKeluar.blade.php
@@ -30,7 +30,7 @@
         <td>Tanggal Pengiriman Barang</td>
     </tr>
     </thead>
-    @foreach(barangKeluar as $data)
+    @foreach($barangKeluar as $data)
         <tbody>
         <tr>
             <td>{{ $no++ }}</td>
