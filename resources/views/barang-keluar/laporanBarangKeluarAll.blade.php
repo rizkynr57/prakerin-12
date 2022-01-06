98939e2343f8a1f130d0f17c647f4db4177ea56a
reign90 <rizky.nurahman48@gmail.com>
2022-01-06 08:42:29 +0700

    Update laporanBarangKeluarAll.blade.php

--- resources/views/barang-keluar/laporanBarangKeluarAll.blade.php
+++ resources/views/barang-keluar/laporanBarangKeluarAll.blade.php
@@ -1,16 +1,16 @@
 <style>
-    #product-keluar {
+    #barang-keluar {
         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
         border-collapse: collapse;
         width: 100%;
     }
-    #product-keluar td, #product-keluar th {
+    #barang-keluar td, #barang-keluar th {
         border: 1px solid #ddd;
         padding: 8px;
     }
-    #product-keluar tr:nth-child(even){background-color: #f2f2f2;}
-    #product-keluar tr:hover {background-color: #ddd;}
-    #product-keluar th {
+    #barang-keluar tr:nth-child(even){background-color: #f2f2f2;}
+    #barang-keluar tr:hover {background-color: #ddd;}
+    #barang-keluar th {
         padding-top: 12px;
         padding-bottom: 12px;
         text-align: left;
@@ -19,8 +19,8 @@
     }
 </style>
 
-<table id="product-keluar" width="100%">
- <a type="button" href="{{ route('barang-keluar.cetakPDF') }} class="btn btn-success">Export Ke PDF</a>
+<table id="barang-keluar" width="100%">
+ <a type="button" href="{{ route('exportPDF.barangKeluarAll') }} class="btn btn-success">Export Ke PDF</a>
     <thead>
     <tr>
         <td>No</td>
