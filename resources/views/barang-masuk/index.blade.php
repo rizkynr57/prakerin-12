4e553d885dd9c2aa2cbb331cd68b347647060f3c
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-07 23:52:01 +0700

    Update index.blade.php

--- resources/views/barang-masuk/index.blade.php
+++ resources/views/barang-masuk/index.blade.php
@@ -52,9 +52,7 @@
                                         @method('DELETE')
                                         <a href="{{ route('barang-masuk.edit', $data->id) }}" class="btn btn-outline-success">Edit</a>
                                         <a href="{{ route('laporanBarangMasuk', $data->id) }}" class="btn btn-outline-warning" target="_blank">Print</a>
-                                        <button type="submit" onclick="return confirm('Apakah anda yakin?')" class="btn btn-outline-danger">
-                                            Delete
-                                        </button>
+                                        @include('barang-masuk.delete')
                                         </tbody>
                                         </form>
                                     </td>
