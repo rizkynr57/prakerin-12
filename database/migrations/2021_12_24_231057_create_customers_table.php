8d857d3a3999164c5f4cb361307c5f3d8de19a07
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-08 11:16:22 +0700

    Update 2021_12_24_231057_create_customers_table.php

--- database/migrations/2021_12_24_231057_create_customers_table.php
+++ database/migrations/2021_12_24_231057_create_customers_table.php
@@ -1,1 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->text('alamat');
+            $table->string('email');
+            $table->string('telepon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
 
