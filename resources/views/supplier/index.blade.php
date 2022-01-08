e20cff2323c43e37a5ab8322fed6cea63d2b52d5
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-08 21:04:53 +0700

    Update index.blade.php

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -46,14 +46,12 @@
                                     <td>{{ $data->no_telp }}</td>
                                     <td>{{ $data->nama_perusahaan }}</td>
                                     <td>
-                                        <form action="{{ route('supplier.destroy', $data->id) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
+                                        
                                         <a href="{{ route('supplier.edit', $data->id) }}" class="btn btn-outline-success">Edit</a>
                                         <a href="{{ route('supplier.show', $data->id) }}" class="btn btn-outline-warning">Show</a>
                                         @include('supplier.delete')
                                         </tbody>
-                                        </form>
+                                        
                                     </td>
                                 </tr>
                             @endforeach
