0deae94c4052e396a5f8e69b6988ceb9561d30ac
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-09 22:36:01 +0700

    Update edit.blade.php

--- resources/views/barang-keluar/edit.blade.php
+++ resources/views/barang-keluar/edit.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade barangKeluar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+<div class="modal fade barangKeluar-edit-" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
