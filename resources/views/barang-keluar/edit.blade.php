a7fa94a723ded11d7acadce685b4fd3cdfe11346
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-09 22:45:05 +0700

    Update edit.blade.php

--- resources/views/barang-keluar/edit.blade.php
+++ resources/views/barang-keluar/edit.blade.php
@@ -8,7 +8,7 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="{{ route('barang-keluar.update', $barangKeluar->id) }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('barang-keluar.update', $data->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="modal-body">
