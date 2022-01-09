3e1f5266c02e19adcfcbd51998db2b2f083c7622
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-09 19:25:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,30 +19,30 @@ Auth::routes(
     ]
 );
 
-Route::get('/home', HomeController@index)->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 // Hak akses admin dan petugas dibatasi melalui controller langsung!!!
 
 Route::group(['middleware' => 'auth'], function(){
    
-    Route::resource('supplier', SupplierController::class);
-    Route::get('/cetak-supplier', SupplierController@cetakSupplierPDF)->name('exportPDF.suppliersAll');
+    Route::resource('supplier', 'SupplierController::class');
+    Route::get('/cetak-supplier', 'SupplierController@cetakSupplierPDF')->name('exportPDF.suppliersAll');
 
-    Route::resource('customer', CustomerController::class);
-    Route::get('/cetak-customer', CustomerController@cetakCustomerPDF)->name('exportPDF.customersAll');
+    Route::resource('customer', 'CustomerController::class');
+    Route::get('/cetak-customer', 'CustomerController@cetakCustomerPDF')->name('exportPDF.customersAll');
 
-    Route::resource('barang', BarangController::class);
+    Route::resource('barang', 'BarangController::class');
 
-    Route::resource('barang-keluar', BarangKeluarController::class);
-    Route::get('/laporan-barangkeluar-all', BarangKeluarController@laporanBarangKeluarAll)->name('laporanBarangKeluarAll');
-    Route::get('/laporan-barangkeluar/{id}', BarangKeluarController@laporanBarangKeluar)->name('laporanBarangKeluar');
-    Route::get('/cetak-pdf-all', BarangKeluarController@cetakPDF_all)->name('exportPDF.barangKeluarAll');
-    Route::get('/cetak-pdf/{id}', BarangKeluarController@cetakPDF)->name('exportPDF.barangKeluar');
+    Route::resource('barang-keluar', 'BarangKeluarController::class');
+    Route::get('/laporan-barangkeluar-all', 'BarangKeluarController@laporanBarangKeluarAll')->name('laporanBarangKeluarAll');
+    Route::get('/laporan-barangkeluar/{id}', 'BarangKeluarController@laporanBarangKeluar')->name('laporanBarangKeluar');
+    Route::get('/cetak-pdf-all', 'BarangKeluarController@cetakPDF_all')->name('exportPDF.barangKeluarAll');
+    Route::get('/cetak-pdf/{id}', 'BarangKeluarController@cetakPDF')->name('exportPDF.barangKeluar');
 
-    Route::resource('barang-masuk', BarangMasukController::class);
-    Route::get('/laporan-barangmasuk-all', BarangMasukController@laporanBarangMasukAll)->name('laporanBarangMasukAll');
-    Route::get('/laporan-barangmasuk/{id}', BarangMasukController@laporanBarangMasuk)->name('laporanBarangMasuk');
-    Route::get('/cetak-pdf-all', BarangMasukController@cetakPDF_all)->name('exportPDF.barangMasukAll');
-    Route::get('/cetak-pdf/{id}', BarangMasukController@cetakPDF)->name('exportPDF.barangMasuk');
+    Route::resource('barang-masuk', 'BarangMasukController::class);
+    Route::get('/laporan-barangmasuk-all', 'BarangMasukController@laporanBarangMasukAll')->name('laporanBarangMasukAll');
+    Route::get('/laporan-barangmasuk/{id}', 'BarangMasukController@laporanBarangMasuk')->name('laporanBarangMasuk');
+    Route::get('/cetak-pdf-all', 'BarangMasukController@cetakPDF_all')->name('exportPDF.barangMasukAll');
+    Route::get('/cetak-pdf/{id}', 'BarangMasukController@cetakPDF')->name('exportPDF.barangMasuk');
 });
 
