d20156d64ad5307d4dbfefa069764444c6376d0f
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-09 22:29:16 +0700

    Update edit.blade.php

--- resources/views/barang-keluar/edit.blade.php
+++ resources/views/barang-keluar/edit.blade.php
@@ -10,6 +10,7 @@
             </div>
             <form action="{{ route('barang-keluar.update', $barangKeluar->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="modal-body">
                     <div class="form-group">
                         <label for="">Nama Customer</label>
