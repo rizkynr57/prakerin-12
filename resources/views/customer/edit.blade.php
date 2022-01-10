f42b4e791dbe1aba0d95fe902cd3d985fa6a4b0b
rizkynr90 <rizky.nurahman48@gmail.com>
2022-01-10 09:26:51 +0700

    Update edit.blade.php

--- resources/views/customer/edit.blade.php
+++ resources/views/customer/edit.blade.php
@@ -1,89 +1,43 @@
-@extends('adminlte::page')
-
-@section('title', 'Data Customer')
-
-@section('content_header')
-
-<h2><br></h2>
-
-@stop
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">Edit Customer</div>
-                <div class="card-body">
-                    <form action="{{ route('customer.update', $supplier->id) }}" method="post">
-                    @method('PUT')
-                    @csrf
+<div class="modal fade customer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Supplier</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('supplier.update', $data->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Kode Customer</label>
+                        <input type="text" class="form-control boxed" name="kode" value="{{ $customer->kode }}" readonly>
+                    </div>
                     <div class="form-group">
                         <label for="">Nama Customer</label>
-                        <input type="text" name="nama" value="{{ $customer->nama }}" class="form-control"
-                        @error('nama')
-                        is-invalid
-                        @enderror>
-                        @error('nama')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                        <input type="text" class="form-control" name="nama" value="{{ $customer->nama }}" required>
                     </div>
                     <div class="form-group">
                         <label for="">Alamat</label>
-                        <input type="text" name="alamat" value="{{ $customer->alamat }}" class="form-control"
-                        @error('alamat')
-                            is-invalid
-                        @enderror>
-                        @error('alamat')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                        <input type="text" class="form-control" name="alamat" value="{{ $customer->alamat }}" required>
                     </div>
                     <div class="form-group">
                         <label for="">Email</label>
-                        <input type="text" name="email" value="{{ $customer->email }}" class="form-control"
-                        @error('email')
-                            is-invalid
-                        @enderror>
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                        <input type="email" class="form-control" name="email" value="{{ $customer->email }}" required>
                     </div>
                     <div class="form-group">
                         <label for="">Nomor Telepon</label>
-                        <input type="text" name="no_telp" value="{{ $customer->no_telp }}" class="form-control"
-                        @error('no_telp')
-                            is-invalid
-                        @enderror>
-                        @error('no_telp')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                        <input type="text" class="form-control" name="no_telp" value="{{ $customer->no_telp }}" required>
                     </div>
-                    <div class="form-group">
-                        <br>
-                        <button type="submit" class="btn btn-outline-primary">Submit</button>
-                        <button type="reset" class="btn btn-outline-warning">Reset</button>
-                    </div>
-                    </form>
                 </div>
-            </div>
+                <div class="modal-footer">
+                    <button type="reset" class="btn btn-sm btn-outline-secondary" data-dismiss="modal">Reset</button>
+                    <button type="submit" class="btn btn-sm btn-outline-primary">Save</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
-@stop
-
-@section('css')
-
-@stop
-
-@section('js')
-
-@stop
 
