7a3dc23efccb6886128b13c07dc28183d875c28d
reign57 <rizky.nurahman48@gmail.com>
2022-01-10 21:40:04 +0700

    Update Home.php

--- app/Models/Home.php
+++ app/Models/Home.php
@@ -1,1 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Session;
+
+class Home extends Model
+{
+    use HasFactory;
+
+    public function InOut()
+    {
+        return $this->hasManyThrough(related:Barang_masuk::class, related:Barang_keluar::class);
+    }
+
+    
+}
 
