2041878e72a390c68ace3279c153c35467f8104d
reign57 <rizky.nurahman48@gmail.com>
2022-01-11 20:11:58 +0700

    Update index.blade.php

--- resources/views/barang-masuk/index.blade.php
+++ resources/views/barang-masuk/index.blade.php
@@ -51,7 +51,7 @@
                                                         data-toggle="modal"
                                                         data-target=".barangMasuk-edit-{{ $data->id }}">Edit
                                                     </a>
-                                                    <a href="{{ route('exportPDF.barangKeluar', $data->id) }}"
+                                                    <a href="{{ route('exportPDF.barangMasuk', $data->id) }}"
                                                         class="btn btn-warning" target="_blank">Print</a>
                                                     <button type="submit" class="btn btn-danger">Delete</button>
                                                 </form>
