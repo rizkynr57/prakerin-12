8f1a00c76fe7202b24c6e4f8bdd29762eae43c22
reign57 <rizky.nurahman48@gmail.com>
2022-01-11 19:42:55 +0700

    Update 2021_12_27_231136_create_barang_keluars_table.php

--- database/migrations/2021_12_27_231136_create_barang_keluars_table.php
+++ database/migrations/2021_12_27_231136_create_barang_keluars_table.php
@@ -21,7 +21,7 @@ class CreateBarangKeluarsTable extends Migration
             $table->foreign('id_barang')->references('id')->on('barangs');
             $table->integer('jumlah_pengiriman');
             $table->integer('harga_satuan');
-            $table->integer('pendapatan');
+            $table->integer('satuan');
             $table->string('tujuan');
             $table->date('tgl_pengiriman');
             $table->timestamps();
