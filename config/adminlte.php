b2c679543f4192b9325df2b5bc98b952af3e0bcf
rizkynr57 <rizky.nurahman48@gmail.com>
2022-01-13 01:28:54 -0800

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
      */
 
-    'logo' => '<br>',
+    'logo' => '<i><B>INDOMARET</B></i>',
     'logo_img' => 'vendor/adminlte/dist/img/indo1.jpeg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
