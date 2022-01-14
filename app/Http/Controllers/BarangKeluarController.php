007757ad4fab4b38ee785b9948a49f72a1e373eb
Reign <rizky.nurahman48@gmail.com>
2022-01-14 21:15:42 +0700

    Update BarangKeluarController.php

--- app/Http/Controllers/BarangKeluarController.php
+++ app/Http/Controllers/BarangKeluarController.php
@@ -90,6 +90,9 @@ class BarangKeluarController extends Controller
         $keluar->save();
 
         $getData['stok_barang'] -= $request->jumlah;
+  //      if (!Barang::where('id', $request->id_barang)->get()->value('stok_barang') >= $request->jumlah) {
+  //            return back()->withError('Tidak boleh melebihi batas sisa');
+  //      }
         $getData->save();
 
         $total = Barang_keluar::find($request->id_barang);
