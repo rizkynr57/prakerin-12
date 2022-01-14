9090596796b3e52666482725c62946c4493628a9
rizkynr57 <rizky.nurahman48@gmail.com>
2022-01-14 15:35:07 +0700

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -12,7 +12,7 @@ class CustomerController extends Controller
 
     public function __construct()
     {
-        $this->middleware('role:admin,petugas');
+        //
     }
 
     public function index()
