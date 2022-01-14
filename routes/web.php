d9f8b57a5bb59623f53f24edd0dabb78a07896d1
Reign <rizky.nurahman48@gmail.com>
2022-01-14 18:36:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,11 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/laporan-barangkeluar-all', [BarangKeluarController::class, 'laporanBarangKeluarAll']);
     Route::get('/laporan-barangkeluar/{id}', [BarangKeluarController::class, 'laporanBarangKeluar'])->name('laporanBarangKeluar');
     Route::get('/cetak-pdf-all', [BarangKeluarController::class, 'cetakPDF_all'])->name('exportPDF.barangKeluarAll');
+    Route::get('/cetak-pdf/{id}', [BarangKeluarController::class, 'cetakPDF'])->name('exportPDF.barangKeluar');
 
     Route::resource('barang-masuk', BarangMasukController::class);
     Route::get('/laporan-barangmasuk-all', [BarangMasukController::class, 'laporanBarangMasukAll']);
     Route::get('/laporan-barangmasuk/{id}', [BarangMasukController::class, 'laporanBarangMasuk'])->name('laporanBarangMasuk');
     Route::get('/cetak-pdf-all', [BarangMasukController::class, 'cetakPDF_all'])->name('exportPDF.barangMasukAll');
+    Route::get('/cetak-pdf/{id}', [BarangMasukController::class, 'cetakPDF'])->name('exportPDF.barangMasuk');
 });
