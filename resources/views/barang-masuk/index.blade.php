816dc90b6af78c7801dc3e27e664d0bb8a9279ce
Reign <rizky.nurahman48@gmail.com>
2022-01-15 06:43:11 +0700

    Update index.blade.php

--- resources/views/barang-masuk/index.blade.php
+++ resources/views/barang-masuk/index.blade.php
@@ -10,6 +10,7 @@
 
 @section('content')
 @include('layouts._flash')
+@role('admin')
     <div class="container">
         <div class="'row">
             <div class="col">
@@ -73,6 +74,66 @@
         </div>
     </div>
     @include('sweetalert::alert')
+ @endrole
+
+@role('petugas')
+<div class="container">
+        <div class="'row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">Data Barang Masuk
+                        <a type="button" style="float: right;" class="btn btn-outline-primary" data-toggle="modal"
+                            data-target=".barangMasuk">Tambah Data</a>
+                        @include('barang-masuk.create')
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table" id="barangMasuk">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Supplier</th>
+                                        <th>Nama Barang</th>
+                                        <th>Jumlah Pemasukan</th>
+                                        <th>Tanggal Masuk Barang</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $no = 1;
+                                    @endphp
+                                    @foreach ($barangMasuk as $data)
+                                        <tr>
+                                            <td>{{ $no++ }}</td>
+                                            <td>{{ $data->supplier->kode }}</td>
+                                            <td>{{ $data->barang->nama_barang }}</td>
+                                            <td>{{ $data->jumlah_pemasukan }}</td>
+                                            <td>{{ $data->tgl_masuk }}</td>
+                                            <td>
+                                                    <a class="btn btn-outline btn-sm btn-outline-warning"
+                                                        data-toggle="modal"
+                                                        data-target=".barangMasuk-edit-{{ $data->id }}">
+                                                     <i class="fas fa-edit"></i> Edit</a>
+                                                    </a>
+                                                    <a href="{{ route('laporanBarangMasuk', $data->id) }}"
+                                                        class="btn btn-warning" target="_blank">
+                                                      <i class="fas fa-print"></i> Print</a>
+                                            </td>
+                                </tbody>
+                                </td>
+                                </tr>
+                                @include('barang-masuk.edit')
+                                @endforeach
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('sweetalert::alert')
+@endrole
 @endsection
 
 @section('css')
