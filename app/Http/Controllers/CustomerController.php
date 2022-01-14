d122373e2d5898c160f02e21d7c7226c91bba97f
Reign <rizky.nurahman48@gmail.com>
2022-01-14 18:53:44 +0700

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -13,6 +13,7 @@ class CustomerController extends Controller
     public function __construct()
     {
         if (Auth::user() == 'petugas') {
+
             return redirect('home')->withError('Anda tidak memiliki akses pada halaman sebelumnya !');
         }
     }
