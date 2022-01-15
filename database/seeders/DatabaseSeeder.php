e53957084a1c9da92d8d01ee0144872fdf5828f6
Reign <rizky.nurahman48@gmail.com>
2022-01-15 12:11:41 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
-            BarangSeeder::class,
-            SupplierSeeder::class
+            UserSeeder::class
         ]);
     }
 }
