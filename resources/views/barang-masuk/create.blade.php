01a50eed54cb4406660f2abe26a8b1e9df83b088
α <rizky.nurahman48@gmail.com>
2022-01-18 06:06:47 +0700

    Update create.blade.php

--- resources/views/barang-masuk/create.blade.php
+++ resources/views/barang-masuk/create.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade barangMasuk" id="form-item" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade barangMasuk" id="modal-form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
