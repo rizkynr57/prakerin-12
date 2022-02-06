efc1d295c1e4c0730898191940e7cfd2bf197086
Vérmíllìøñ <rizky.nurahman48@gmail.com>
2022-02-06 08:04:52 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,26 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Supplier;
+use App\Models\Customer;
+use App\Models\Barang_masuk;
+use App\Models\Barang_Keluar;
+use App\Models\Barang;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+ 
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $supplier = Supplier::count();
+        $customer = Customer::count();
+        $masuk = Barang_masuk::count();
+        $keluar = Barang_keluar::count();
+        $barang = Barang::all();
+        return view('home', compact('supplier', 'customer', 'masuk', 'keluar', 'barang'));
     }
 }
