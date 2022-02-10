4ae9a16bd329b48e64dae8d2d98d865635c446b8
rizkynr57 <rizky.nurahman48@gmail.com>
2022-02-10 10:46:07 +0700

    Update index.blade.php

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -7,11 +7,9 @@
 @endsection
 
 @section('content')
-    @include('layouts._flash')
     <div class="container">
         <div class="row">
             <div class="col">
-
                 <div class="card">
                     <div class="card-header">
                         <div style="float: left;">
