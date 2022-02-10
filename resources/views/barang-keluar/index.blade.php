289fe468c47ddb1372d3ddbea1bf38d88490265d
rizkynr57 <rizky.nurahman48@gmail.com>
2022-02-10 16:32:18 +0700

    Update index.blade.php

--- resources/views/barang-keluar/index.blade.php
+++ resources/views/barang-keluar/index.blade.php
@@ -57,7 +57,7 @@
                                                         data-target=".barangKeluar-edit-{{ $data->id }}">
                                                      <i class="fas fa-edit"></i></a>
                                                     </a>
-                                                    <button type="submit" class="btn btn-danger btn-sm rounded-circle" onclick="return confirm('Are You Sure ?')">
+                                                    <button type="submit" class="btn btn-danger btn-sm rounded-circle" id="delete-confirm">
                                                      <i class="fas fa-trash"></i></button>
                                                 </form>
                                             </td>
@@ -146,4 +146,26 @@
             $('#barangKeluar').DataTable();
         });
     </script>
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    $(".delete-confirm").click(function (event) {
+        var form = $(this).closest("form");
+        var name = $(this).data("name");
+        event.preventDefault();
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit();
+            }
+        });
+    });
+</script>
 @endsection
