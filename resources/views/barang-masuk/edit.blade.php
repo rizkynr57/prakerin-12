83770e8be295ae1c12f371b4af291c4683b5b548
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-01 21:04:50 +0700

    Update edit.blade.php

--- resources/views/barang-masuk/edit.blade.php
+++ resources/views/barang-masuk/edit.blade.php
@@ -24,10 +24,6 @@
                         <label for="">Jumlah Barang</label>
                         <input type="text" class="form-control" name="jumlah" value="{{ $data->jumlah_pemasukan }}"required>
                     </div>
-                    <div class="form-group">
-                        <label for="">Tanggal Barang Masuk</label>
-                        <input type="date" name="tgl_masuk" class="form-control" value="{{ $data->tgl_masuk }}" required>
-                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-info">Save</button>
