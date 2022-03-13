232e1df30327b57a12c106461841ff47243a4ec9
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-13 21:46:44 +0700

    Update index.blade.php

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -58,7 +58,7 @@
                                                 <a class="btn btn-info btn-sm rounded-circle" data-toggle="modal"
                                                     data-target=".supplier-show-{{ $data->id }}">
                                                         <i class="fas fa-id-card"></i></a>
-                                                <button class="btn btn-danger btn-sm rounded-circle">
+                                                <button type="submit" class="btn btn-danger btn-sm rounded-circle" onclick="return confirm('Are you sure?');">
                                                 <i class="fas fa-trash"></i></button>
                                             </form>
                                         </tbody>
