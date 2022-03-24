8193a137ebd6aaba82e72ad3a6ce65f8fbc0231b
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 13:40:46 +0700

    Update ApiSupplierController.php

--- app/Http/Controllers/API/ApiSupplierController.php
+++ app/Http/Controllers/API/ApiSupplierController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 
-class BarangKeluarController extends Controller
+class ApiSupplierController extends Controller
 {
 
     public function index()
