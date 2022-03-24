1f1766bf938d8e93920e41f159f22ae6848de96a
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 12:04:55 +0700

    getx

---
+++ app/Http/Controllers/API/ApiCustomerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+
+    public function index()
+    {
+        $customer = Customer::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Customer',
+            'data' => $customer,
+        ], 201);
+    }
+
+    public function store(Request $request)
+    {
+        $customer = new Customer;
+        $customer->kode = $request->kode;
+        $customer->nama = $request->nama;
+        $customer->alamat = $request->alamat;
+        $customer->email = $request->email;
+        $customer->telepon = $request->no_telp;
+        $customer->save();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Customer',
+            'data' => $customer,
+        ], 201);
+
+    }
+
+    public function show($id)
+    {
+
+        $customer = Customer::find($id);
+        if ($customer) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Show Data Customer',
+                'data' => $customer,
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data Customer tidak ditemukan',
+                'data' => [],
+            ], 404);
+
+        }
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::findOrfail($id);
+        $customer->nama = $request->nama;
+        $customer->alamat = $request->alamat;
+        $customer->email = $request->email;
+        $customer->telepon = $request->no_telp;
+        $customer->save();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Kategori Berhasil diedit',
+            'data' => $kategori,
+        ], 201);
+    }
+
+    public function destroy($id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data customer Berhasil hapus',
+            'data' => $customer,
+        ], 200);
+
+    }
+}
---
+++ app/Http/Controllers/API/ApiSupplierController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+
+class BarangKeluarController extends Controller
+{
+
+    public function index()
+    {
+        $supplier = Supplier::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Supplier',
+            'data' => $supplier,
+        ], 201);
+    }
+
+    public function store(Request $request)
+    {
+        $supplier = new Supplier();
+        $supplier->kode = $request->kode;
+        $supplier->nama_supplier = $request->nama;
+        $supplier->alamat = $request->alamat;
+        $supplier->no_telp = $request->no_telp;
+        $supplier->nama_perusahaan = $request->perusahaan;
+        $supplier->save();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Supplier',
+            'data' => $supplier,
+        ], 201);
+    }
+
+    public function show($id) 
+    {
+        $supplier = Supplier::find($id);
+        if ($supplier) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Show Data Supplier',
+                'data' => $supplier,
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data Supplier tidak ditemukan',
+                'data' => [],
+            ], 404);
+
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->kode = $request->kode;
+        $supplier->nama_supplier = $request->nama;
+        $supplier->alamat = $request->alamat;
+        $supplier->no_telp = $request->no_telp;
+        $supplier->nama_perusahaan = $request->perusahaan;
+        $supplier->save();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Kategori Berhasil diedit',
+            'data' => $supplier,
+        ], 201);
+
+    }
+
+    public function destroy($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Supplier Berhasil hapus',
+            'data' => $supplier,
+        ], 200);
+    }
+}
