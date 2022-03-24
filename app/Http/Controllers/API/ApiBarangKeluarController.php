ae6c77f8c148b83a197788b1e312db43f80bfb62
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-24 13:34:54 +0700

    Create ApiBarangKeluarController.php

---
+++ app/Http/Controllers/API/ApiBarangKeluarController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang_keluar;
+use Illuminate\Http\Request;
+
+class ApiBarangKeluarController extends Controller
+{
+
+    public function index()
+    {
+        $barangKeluar = Barang_keluar::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Barang Masuk',
+            'data' => $barangKeluar,
+        ], 201);
+    }
+
+    public function store(Request $request)
+    {
+          $barangkeluar = new Barang_keluar();
+          $barangkeluar->id_customer = $request->id_customer;
+          $barangkeluar->id_barang = $request->id_barang;
+          $barangkeluar->jumlah_pengiriman = $request->jumlah;
+          $getData = Barang::findOrFail($request->id_barang);
+          $barangkeluar->harga_satuan = $getData['harga_jual'];
+          $barangkeluar->tgl_pengiriman = $request->tgl_pengiriman;
+          $barangkeluar->tujuan = $request->tujuan;
+          $barangkeluar->save();
+
+          $getData->stok_barang -= $request->jumlah;
+          $getData->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Barang Keluar',
+            'data' => $barangKeluar,
+        ], 201);
+
+    }
+
+    public function show($id)
+    {
+
+        $barangKeluar = Barang_keluar::find($id);
+        if ($barangKeluar) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Show Data Barang Keluar',
+                'data' => $barangKeluar,
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data Barang Keluar tidak ditemukan',
+                'data' => [],
+            ], 404);
+
+        }
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $barangKeluar = Barang_keluar::findOrFail($id);
+        $reset = Barang::findOrFail($request->id_barang);
+        $reset['stok_barang'] += $barangKeluar['jumlah_pengiriman'];
+        $reset->save();
+
+        $barangKeluar->jumlah_pengiriman = $request->jumlah;
+        $barangKeluar->tujuan = $request->tujuan;
+        $barangKeluar->save();
+
+        $barang = Barang::findOrFail($request->id_barang);
+        $barang['stok_barang'] -= $request->jumlah;
+        $barang->save();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Barang Keluar Berhasil diedit',
+            'data' => $barangKeluar,
+        ], 201);
+    }
+
+    public function destroy($id)
+    {
+        $barangKeluar = Barang_keluar::findOrFail($id);
+        $barangKeluar->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Barang Masuk Berhasil hapus',
+            'data' => $barangKeluar,
+        ], 200);
+
+    }
+}
