2947016393fb770739dc9a3a450a52011e18a5a4
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-26 17:59:32 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,1 +1,31 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use App\Models\Barang_masuk;
+use App\Models\Barang_keluar;
+use DB;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+
+    public function JoinBarangMasuk()
+    {
+        $barangMasuk = Barang_masuk::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Barang Masuk',
+            'data' => $barangMasuk,
+        ], 201);
+    }
+
+    public function JoinBarangKeluar()
+    {
+        $barangKeluar = Barang_keluar::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Barang Keluar',
+            'data' => $barangKeluar,
+        ], 201);
+    }
