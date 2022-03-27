103164c47ed8509ebdda8436a5c66a00cb340c3f
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-27 23:03:47 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\ApiBarangKeluarController;
 use App\Http\Controllers\ApiBarangMasukController;
