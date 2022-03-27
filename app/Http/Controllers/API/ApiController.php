799da2055f50269a08575edf17b8d6e3bef8d5f6
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-27 23:09:33 +0700

    Update ApiController.php

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -11,28 +11,28 @@ class ApiController extends Controller
     public function JoinBarangMasuk()
     {
         $barangMasuk = DB::table('barang_masuks')
-        // ->join('suppliers', 'barang_masuks.id_supplier', '=', 'barang_masuk.id_supplier')
+        ->join('suppliers', 'barang_masuks.id_supplier', '=', 'barang_masuk.id_supplier')
         ->join('barangs', 'barang_masuks.id_barang', '=', 'barang_masuk.id_barang')
-        ->select('barangs.nama_barang as barang', 'barang_masuks.jumlah_pemasukan', 'barang_masuks.tgl_masuk')
+        ->select('suppliers.nama_supplier', 'barangs.nama_barang as barang', 'barang_masuks.jumlah_pemasukan', 'barang_masuks.tgl_masuk')
         ->get();
         return response()->json([
             'success' => true,
             'message' => 'Data Barang Masuk',
             'data' => $barangMasuk,
-        ], 200);
+        ], 201);
     }
 
     public function JoinBarangKeluar()
     {
         $barangKeluar = DB::table('barang_keluars')
-        // ->join('customers', 'barang_keluars.id_customer', 'barang_keluar.id_customer')
+        ->join('customers', 'barang_keluars.id_customer', 'barang_keluar.id_customer')
         ->join('barangs', 'barang_keluars.id_barang', '=', 'barang_keluar.id_barang')
-        ->select('barangs.nama_barang as barang', 'barang_keluars.jumlah_pengiriman', 'barang_keluars.harga_satuan', 'barang_keluars.tujuan', 'barang_keluars.tgl_pengirimam')
+        ->select('customers.nama', 'barangs.nama_barang as barang', 'barang_keluars.jumlah_pengiriman', 'barang_keluars.harga_satuan', 'barang_keluars.tujuan', 'barang_keluars.tgl_pengirimam')
         ->get();
         return response()->json([
             'success' => true,
             'message' => 'Data Barang Keluar',
             'data' => $barangKeluar,
-        ], 200);
+        ], 201);
     }
 }
