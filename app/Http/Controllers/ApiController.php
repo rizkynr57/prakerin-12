40c2acf00c8670f866d454df46b2a269906de0f7
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-27 22:35:55 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Barang_masuk;
 use App\Models\Barang_keluar;
 use App\Models\Barang;
+use App\models\Supplier;
+use App\Models\Customer;
 use DB;
 use Illuminate\Http\Request;
 
@@ -14,6 +16,7 @@ class ApiController extends Controller
     public function JoinBarangMasuk()
     {
         $barangMasuk = DB::table('barang_masuks')
+        // ->join('suppliers', 'barang_masuks.id_supplier', '=', 'barang_masuk.id_supplier')
         ->join('barangs', 'barang_masuks.id_barang', '=', 'barang_masuk.id_barang')
         ->select('barangs.nama_barang as barang', 'barang_masuks.jumlah_pemasukan', 'barang_masuks.tgl_masuk')
         ->get();
@@ -26,10 +29,15 @@ class ApiController extends Controller
 
     public function JoinBarangKeluar()
     {
-        $barangKeluar = Barang_keluar::all();
+        $barangKeluar = DB::table('barang_keluars')
+        // ->join('customers', 'barang_keluars.id_customer', 'barang_keluar.id_customer')
+        ->join('barangs', 'barang_keluars.id_barang', '=', 'barang_keluar.id_barang')
+        ->select('barangs.nama_barang as barang', 'barang_keluars.jumlah_pengiriman', 'barang_keluars.harga_satuan', 'barang_keluars.tujuan', 'barang_keluars.tgl_pengirimam')
+        ->get();
         return response()->json([
             'success' => true,
             'message' => 'Data Barang Keluar',
             'data' => $barangKeluar,
         ], 201);
     }
+}
