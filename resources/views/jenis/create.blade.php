39c4b0093d8dd9fe847f9c55f9394882a54eb3e5
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 23:20:42 +0700

    Update create.blade.php

--- resources/views/jenis/create.blade.php
+++ resources/views/jenis/create.blade.php
@@ -1,1 +1,26 @@
 
+<div class="modal fade jenis" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Supplier</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('jenis.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Nama Jenis</label>
+                        <input type="text" class="form-control" name="jenis" id="jenis" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="reset" class="btn btn-secondary" data-dismiss="modal">Reset</button>
+                    <button type="submit" class="btn btn-info">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
