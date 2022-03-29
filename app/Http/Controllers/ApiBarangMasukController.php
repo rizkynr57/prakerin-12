229d9194451c00b2a9d00017a6e63af29ab80bc8
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 12:11:56 +0700

    Update ApiBarangMasukController.php

--- app/Http/Controllers/ApiBarangMasukController.php
+++ app/Http/Controllers/ApiBarangMasukController.php
@@ -10,7 +10,12 @@ class ApiBarangMasukController extends Controller
 
     public function index()
     {
-        $barangMasuk = Barang_masuk::all();
+        // $barangMasuk = Barang_masuk::all();
+        $barangMasuk = DB::table('barang_masuks')
+        ->join('suppliers', 'barang_masuks.id_supplier', '=', 'barang_masuk.id_supplier')
+        ->join('barangs', 'barang_masuks.id_barang', '=', 'barang_masuk.id_barang')
+        ->select('suppliers.nama_supplier as leveransir', 'barangs.nama_barang as barang', 'barang_masuks.jumlah_pemasukan', 'barang_masuks.tgl_masuk')
+        ->get();
         return response()->json([
             'success' => true,
             'message' => 'Data Barang Masuk',
