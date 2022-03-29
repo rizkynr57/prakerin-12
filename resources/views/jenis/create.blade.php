8a7823d35145cac1a4bca707dc50ad282bffbe66
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 23:32:17 +0700

    Update create.blade.php

--- resources/views/jenis/create.blade.php
+++ resources/views/jenis/create.blade.php
@@ -1,9 +1,8 @@
-
 <div class="modal fade jenis" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary">
-                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Supplier</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Jenis</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
