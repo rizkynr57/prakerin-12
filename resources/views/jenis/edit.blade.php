79fd004b84b9efb7c34dffc68ed36ad20c65e988
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 23:23:58 +0700

    Create edit.blade.php

---
+++ resources/views/jenis/edit.blade.php
@@ -0,0 +1,27 @@
+<div class="modal fade jenis-edit-{{ $data->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-secondary">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Data Supplier</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('jenis.update', $data->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Nama Satuan</label>
+                        <input type="text" class="form-control" name="satuan"
+                            value="{{ $data->nama_satuan }}" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-info">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
