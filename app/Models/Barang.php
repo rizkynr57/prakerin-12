3fdb35e4f2d8ad5f4c825186e4dd2af499f7660c
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 23:57:54 +0700

    Update Barang.php

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -28,4 +28,24 @@ class Barang extends Model
     {
         return $this->hasMany('App\Models\Barang_masuk', 'id_barang');
     }
+
+    public static function code()
+    {
+        $kode = DB::table('barangs')->max('kode');
+        $addNol = '';
+        $kode = str_replace("BRG", "", $kode);
+        $kode = (int) $kode + 1;
+        $incrementKode = $kode;
+
+        if (strlen($kode) == 1) {
+            $addNol = "000";
+        } elseif (strlen($kode) == 2) {
+            $addNol = "00";
+        } elseif (strlen($kode == 3)) {
+            $addNol = "0";
+        }
+
+        $kodeBaru = "BRG" . $addNol . $incrementKode;
+        return $kodeBaru;
+    }
 }
