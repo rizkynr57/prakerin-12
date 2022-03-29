f46f6027dbcb066c6c21f09cbed6d26626b1a651
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 23:32:57 +0700

    Create create.blade.php

---
+++ resources/views/satuan/create.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade satuan" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Satuan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('satuan.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Nama Satuan</label>
+                        <input type="text" class="form-control" name="satuan" id="satuan" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="reset" class="btn btn-secondary" data-dismiss="modal">Reset</button>
+                    <button type="submit" class="btn btn-info">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
