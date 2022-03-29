f7685088c370204fb3d1ef8e962261aaa777fd55
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-29 23:16:17 +0700

    Create SatuanController.php

---
+++ app/Http/Controllers/SatuanController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Satuan;
+use Illuminate\Http\Request;
+
+class SatuanController extends Controller
+{
+
+    public function index()
+    {
+        $satuan = Satuan::all();
+        return view('satuan.index', compact('satuan'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'satuan' => 'required',
+        ]);
+        $satuan = new Satuan();
+        $satuan->nama_satuan = $request->satuan;
+        return redirect('satuan')->withSuccess('Data Berhasil Disimpan!');
+    }
+
+    public function index($id)
+    {
+        $satuan = Satuan::find($id);
+        return view('satuan.show', compact('satuan'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'satuan' => 'required',
+        ]);
+
+        $satuan = Satuan::findOrFail($id);
+        $satuan->nama_satuan = $request->satuan;
+        $satuab->update();
+
+        return redirect('jenis')->withInfo('Data berhasil diubah!');
+    }
+
+    public function destroy($id)
+    {
+        $satuan = Satuan::find($id);
+        if (!Satuan::destroy($id)) {
+            return redirect()->back();
+        }
+        return redirect('satuan')->withSuccess('Data '.$satuan->nama_satuan. ' telah dihapus');
+    }
+}
